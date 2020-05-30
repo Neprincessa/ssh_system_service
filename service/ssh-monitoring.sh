05c21767e1da44fc704827419db80f0dd919e9e0
Худоярова Анастасия Михайловна <asya-iriska@yandex.ru>
2020-05-30 21:43:59 +0300

    [fix]: correct structure and rename

---
+++ service/ssh-monitoring.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source monitor_ssh_lib.sh
+
+PREV_USER=$(ps -eo command | grep "^sshd:.*@.*$" | awk -F'[ @]' '{print $2}')
+
+trap "sleep 30; logger SSH service catched USR1 signal; logger SSH service stopped; echo 'SSH service stopped';  exit 0"  SIGUSR1
+logger "SSH service started"
+echo "SSH service started"
+
+while true
+do
+	NEW_USER=$(ps -eo command | grep "^sshd:.*@.*$" | awk -F'[ @]' '{print $2}')
+	TMP=$(check_ssh_users "$PREV_USER" "$NEW_USER")
+	if [ -n "$TMP" ]	
+	then
+		PREV_USER=$NEW_USER
+		echo "$TMP successfully login"
+		logger $TMP successfully login
+		notify-send "$TMP successfully login"
+	fi
+	sleep 30
+done
+ 
