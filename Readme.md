a640e716565c67634955a4ef906fdec84ae757ea
Худоярова Анастасия Михайловна <asya-iriska@yandex.ru>
2020-05-30 04:08:36 +0300

    [bugfix]: correct view users

--- Readme.md
+++ Readme.md
@@ -2,8 +2,8 @@
 1. Copy service to /etc/systemd/sytem
 2. Copy script to в /usr/bin/ + make executable
 3. systemctl daemon-reload
-4. systemctl enable 
-5. sudo systemctl start monitoring_ssh_service.service 
+4. systemctl enable  monitoring_ssh_service.service
+5. sudo systemctl start monitoring_ssh_service.service <br/> 
 6.sudo systemctl status monitoring_ssh_service.service
 7. systemctl stop monitoring_ssh_service.service - остановка
 8. journalctl -f -u monitoring_ssh_service.service - получаем выводы сервиса
--- monitoring_ssh.sh
+++ monitoring_ssh.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-PREV_USER=$(ps aux | grep ssh | grep @ | tr -s ' ' ' ' | cut -d":" -f4 | cut -d'@' -f1)
-echo "$PREV_USER has been succesfully login"
+PREV_USER=$(ps -eo command | grep "^sshd:.*@.*$" | awk -F'[ @]' '{print $2}')
+echo "SSH service started"
 
 while true
 do
-    NEW_USER=$(ps aux | grep ssh | grep @ | tr -s ' ' ' ' | cut -d":" -f4 | cut -d'@' -f1)
-    RES=$(diff <( echo "$PREV_USER") <( echo "$NEW_USER") |  awk '{print $2}' )
+    NEW_USER=$(ps -eo command | grep "^sshd:.*@.*$" | awk -F'[ @]' '{print $2}')
+    RES=$(diff <( echo "$PREV_USER") <( echo "$NEW_USER") |  grep ">" )
     if [ -n "$RES" ]
     then
-        echo "$RES"
+        echo "$RES has been successfully login"
         notify-send "$RES has been succesfully login"
     fi
     PREV_USER=$NEW_USER
