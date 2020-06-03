a8579d8c164796f3bb5b68fdb6a06f217fe7820a
cheraten <cheraten@yandex.ru>
2020-06-03 16:16:07 +0300

    IvanovaN - RPM package 1.3 with selinux policy

---
+++ rpm_and_repo/rpmbuild/BUILD/ssh-monitoring-1.3/ssh-monitoring.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source ssh-monitoring-lib.sh
+
+PREV_USER=$(ps -eo command | grep "^sshd:.*@.*$" | awk -F'[ @]' '{print $2}')
+
+trap "sleep 30; logger SSH service catched USR1 signal; logger SSH service stopped; exit 0"  SIGUSR1
+logger "SSH service started"
+
+while true
+do
+	NEW_USER=$(ps -eo command | grep "^sshd:.*@.*$" | awk -F'[ @]' '{print $2}')
+	TMP=$(check_ssh_users "$PREV_USER" "$NEW_USER")
+	if [ -n "$TMP" ]	
+	then
+		PREV_USER=$NEW_USER
+		logger $TMP successfully login
+		notify-send "$TMP successfully login"
+	fi
+	sleep 30
+done
+ 
