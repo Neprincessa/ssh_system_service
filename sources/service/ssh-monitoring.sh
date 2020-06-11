a732c1cf29ec20489c2508c0e8cf8d9ba5bf8665
Худоярова Анастасия Михайловна <asya-iriska@yandex.ru>
2020-06-11 15:19:03 +0300

    [fix]: signal

--- sources/service/ssh-monitoring.sh
+++ sources/service/ssh-monitoring.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-source ssh-monitoring-lib.sh
+source monitor_ssh_lib.sh
 
 logger "SSH service started"
 
-PREV_USER=$(ps -eo command | grep "^sshd:.*@.*$" | awk -F'[ @]' '{print $2}')
-
-sleep 50
+PREV_USER=$(ps -eo command | grep "^sshd:.*@.*$" | awk -F'[ @]' '{print $2}') 
 
 trap "logger SSH service catched USR1 signal;  exit 0"  SIGUSR1
 
 while true
 do
+
 	NEW_USER=$(ps -eo command | grep "^sshd:.*@.*$" | awk -F'[ @]' '{print $2}')
 	TMP=$(check_ssh_users "$PREV_USER" "$NEW_USER")
 	if [ -n "$TMP" ]	
@@ -21,5 +20,8 @@ do
 		echo "$TMP successfully login" | wall
 		echo "$TMP successfully login"
 	fi
-	sleep 30
+
+	sleep 30 & 
+	wait
 done
+ 
